e6c0348809fac2d5b557e798dde5da074fa7fa5f
Shubh Harde <shubhharde158@gmail.com>
2024-04-17 23:27:36 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_cors import CORS
+from Verification import WorkerVerification
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+api.add_resource(WorkerVerification, '/verification')
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
